c854408f9d55456ceb8f6179590c63476da4c865
Omkar Pawar <omkar@Omkars-Mac-mini.local>
2025-10-07 21:41:45 +0530

    Get video IDS function

--- video_stats.py
+++ video_stats.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 api_key=os.getenv("api_key")
 channel_handle="MrBeast"
-
+max_results=50
 def get_playlist_id():
     try:
         url=f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={api_key}'
@@ -21,5 +21,31 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlisId):
+    video_ids=[]
+    pageToken=None
+    base_url=f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlistId}&key={api_key}"
+
+    try:
+        while True:
+            url=base_url
+            if pageToken:
+                url+=f"&pageToken={pageToken}"
+            response=requests.get(url)
+            response.raise_for_status()
+            data=response.json()
+
+            for item in data.get('items',[]):
+                video_id=item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken=data.get('nextPageToken')
+            if not pageToken:
+                break
+        return video_ids
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__=="__main__":
-    get_playlist_id()
+    playlistId=get_playlist_id()
+    get_video_ids(playlistId)
