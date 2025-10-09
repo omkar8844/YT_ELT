7ded46d13acc17d77addfee7cb6362c1e604c7f0
Omkar Pawar <omkar@Omkars-Mac-mini.local>
2025-10-09 15:24:56 +0530

    saved file to json

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import json
+from datetime import date
 from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 api_key=os.getenv("api_key")
@@ -82,7 +84,15 @@ def extract_video_data(video_ids):
         return extrcated_data
     except requests.exceptions.RequestException as e:
         raise e
+    
+
+def save_to_json(extracted_data):
+    file_path=f'./data/YT_data_{date.today()}.json'
+    with open(file_path,'w',encoding="utf-8") as json_outfile:
+        json.dump(extracted_data,json_outfile,indent=4,ensure_ascii=False )
+
 if __name__=="__main__":
     playlistId=get_playlist_id()
     video_ids=get_video_ids(playlistId)
-    extract_video_data(video_ids)
+    video_data=extract_video_data(video_ids)
+    save_to_json(video_data)
