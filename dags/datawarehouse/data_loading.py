b927709b8efb7b54248d87ae1ce85bb45faa9d95
Omkar Pawar <omkar@Omkars-Mac-mini.local>
2025-10-17 11:36:19 +0530

    Load and Transform Code

---
+++ dags/datawarehouse/data_loading.py
@@ -0,0 +1,21 @@
+import json
+from datetime import date
+import logging
+
+logger=logging.getLogger(__name__)
+
+def load_data():
+    file_path=f".data/YT_data_{date.today()}.json"
+
+    try:
+        logger.info(f"Processing file: {file_path}")
+
+        with open(file_path,'r',encoding='utf-8') as raw_data:
+            data= json.load(raw_data)
+        return data
+    except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
+        raise
+    except json.JSONDecodeError:
+        logging.error(f"Invalid json file: {file_path}")
+        raise
